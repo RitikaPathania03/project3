3792178c258b2c064ed3677c3a86fdea1ab811db
ritika <ritikaPathania520@gmail.com>
2022-07-12 04:26:27 +0530

    adding all review api's

--- src/controller/reviewsController.js
+++ src/controller/reviewsController.js
@@ -1,4 +1,4 @@
-const reviewModel=require("../module/reviewModel");
+const reviewModel=require("../model/reviewModel");
 const bookModel=require("../model/bookModel")
 const ObjectId = require('mongoose').Types.ObjectId
 
@@ -15,7 +15,7 @@ const reviews=async function (req,res){
     try{
     let bookId=req.params.bookId
     let reviewsData=req.body
-    let{reviewedBy, rating, review, ...rest}=req.body
+    let {reviewedBy, rating, review, ...rest}=req.body
     //check body is empty or not
     if(!Object.keys(reviewsData).length) return res.status(400).send({status:false,message:"Please Enter the Data in Request Body"})
     //check if any unwanted keys present or 
@@ -27,7 +27,7 @@ const reviews=async function (req,res){
    
     if(!rating) return res.status(400).send({status:false, message:"please enter rating"})
 
-    if (typeof rating !== Number) { res.status(400).send({ msg: 'rating should be number type' }) }
+   // if (typeof rating !== Number) { res.status(400).send({ msg: 'rating should be number type' }) }
 
     //check the bookId is Valid or Not ?
     
@@ -74,45 +74,79 @@ const reviews=async function (req,res){
     }
 
 
-const updateReview= async function(req,res){
-    try{
-     let newData=req.body;
-     let bookId=req.params.bookId;
-     let reviewId=req.params.reviewId;
 
-     let{reviewedBy, rating, review, ...rest }= newData 
-     if(!Object.keys(newData).length) return res.status(400).send({status:false,message:"Please Enter the Data in Request Body"})
-     if(!Object.keys(req.params).length) return res.status(400).send({status:false,message:"Please Enter the Data in params"})
-     if(!Object.keys(bookId).length) return res.status(400).send({status:false,message:"Please Enter some bookId in params"})
-     if(!Object.keys(reviewId).length) return res.status(400).send({status:false,message:"Please Enter some reviewId in params"})
+const updateReview = async function (req, res) {
+    try {
+        // Stores the blog id data recieved in params in to a new variable
+        let bookId = req.params.bookId;
+        let reviewId=req.params.reviewId;
+        let newData=req.body;
+        let {reviewedBy, rating, review, ...rest }=newData;
+
+        if(!Object.keys(bookId).length) return res.status(400).send({status:false,message:"Please Enter some bookId in params"})
+        if(!Object.keys(reviewId).length) return res.status(400).send({status:false,message:"Please Enter some reviewId in params"})
+        if(!Object.keys(req.body).length) return res.status(400).send({status:false,message:"Please Enter the Data in Request Body"})
+        if(!Object.keys(req.params).length) return res.status(400).send({status:false,message:"Please Enter the Data in params"})
+        if(reviewedBy){if(!/^[a-zA-Z]+/.test(reviewedBy))  return res.status(400).send({ status: false, msg: "reviewBy should be in alphabets" })};
+        if(review){if(!/^[a-zA-Z]+/.test(review))  return res.status(400).send({ status: false, msg: "review should be in letters" })};
+        if(rating){if(!/^[1-5]$/.test(rating)) return res.status(400).send({ status: false, msg: "rating should be between 1 and 5" })};
+        if(Object.keys(rest).length>0) return res.status(400).send({status:false,message:"Only reviewedBy or rating or review can be updated"})
+
+        let findBook=await bookModel.findOne({_id:bookId, isDeleted:false});
+        if(!findBook){res.status(400).send({status:false,msg:"No such book exists or is deleted"})}
+        let findReview=await reviewModel.findOne({_id:reviewId, isDeleted:false});
+        if(!findReview){res.status(400).send({status:false,msg:"No such review exists or is deleted"})}
+        
+        if(findReview.bookId!=bookId){ res.status(400).send({status:false,msg:"the review doesnt belong to this book or vice versa,change the given id"})}
+             
+        let updateData = await reviewModel.findByIdAndUpdate(reviewId, {
+        review: review, 
+        rating: rating,
+        reviewedBy :reviewedBy,
+       
+        }, { new: true })
+        return res.status(200).send({ status: true,msg:"Success", data: updateData })
+        
+        }
+        catch (err) {
+        return res.status(500).send({ msg: "Serverside Errors. Please try again later", error: err.message })
+        }
+        }
 
 
-     let findBook=await bookModel.findOne(bookId, isDeleted=false);
-     if(!findBook){res.status(400).send({status:false},{msg:"No such book exists or is deleted"})}
-     let findReview=await reviewModel.findOne(reviewId, isDeleted=false);
-     if(!findReview){res.status(400).send({status:false},{msg:"No such review exists or is deleted"})}
-
-     if(!findBook==findReview){res.status(400).send({status:false},{msg:"the review doesnt belong to this book or vice versa,change the given id"})}
-
-     if(reviewedBy){if(typeof reviewedBy !== 'string') { res.status(400).send({ msg: 'reviewedBy should be string type' }) };     
-     findReview.reviewedBy=reviewedBy;}
-
-     if(rating){if(!/^[1-5]$/.test(rating)) return res.status(400).send({ status: false, msg: "rating is Invalid" });
-    
-     findReview.rating=rating;}
-
-     if(review){if(!/^[a-zA-Z]+/.test(review))  return res.status(400).send({ status: false, msg: "review is Invalid" });
-     findReview.review=review;}
-
-     if(rest){res.status(400).send({status:false,msg:"Only reviewedBy or rating or review can be updated"})}
-      
-      
-     let updatedData=await reviewModel.findOneAndUpdate(reviewId,findReview,{new:true});
-     res.status(201).send({status:true, msg:"success",data:updatedData });
-     }catch(err){res.status(500).send({status:false, msg: 'SERVER ERROR', error: err.message });
-    }
-    };
-     
+    const deleteReview=async function(req,res){
+        try{
+        let bookId=req.params.bookId
+        let reviewId = req.params.reviewId
+        let book=await bookModel.findById(bookId)
+        
+        if(!book) return res.status(400).send({status:false, message:"Book is not present in DB"})
+        
+        if(book.isDeleted==true)return res.status(404).send({ status: false, message:"the book is already deleted"})
+        
+        let review=await reviewModel.findById(reviewId)
+          
+        
+        if(!review) return res.status(400).send({status:false, message:"No review found for this book"})
+        
+        if(review.isDeleted==true)return res.status(404).send({ status: false, message:"This review is Deleted."})
+        
+        if(review.bookId != bookId) return res.status(400).send({status:false, message : "the review doesnt belong to this book or vice versa,change the given id"})
+           
+            //update the status of isDeleted to TRUE
+        let updatedData = await reviewModel.findOneAndUpdate({ _id: reviewId }, {isDeleted:true }, { new: true });
+        book.reviews--;
+        await book.save();
+        
+        return res.status(200).send({ status: true, msg: "Successfuly Deleted" });  
+        }
+        catch(error){
+        return res.status(500).send({ msg: "Serverside Errors. Please try again later", error: error.message })
+        
+        }
+        
+        }
    
 module.exports.reviews=reviews;
 module.exports.updateReview=updateReview;
+module.exports.deleteReview=deleteReview;
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const userController=require("../controller/userController")
 const bookController=require("../controller/bookController")
 const middleware = require("../middleware/userAuthMiddleware")
-//const authorisationMW = require("../middleware/userAuthMiddleware")
+const reviewsController=require("../controller/reviewsController")
+
 
 
 router.post("/register", userController.userRegistartion)
@@ -16,13 +17,17 @@ router.get("/books",middleware.authentication, bookController.getBooks)
 
 router.put("/books/:bookId", bookController.updateBooks)
 
- router.get("/books/:bookId",middleware.authentication, bookController.getBooksById)
+router.get("/books/:bookId",middleware.authentication, bookController.getBooksById)
 
- router.delete("/books/:bookId" , middleware.authentication  , middleware.authorisation, bookController.deleteBook)
+router.delete("/books/:bookId" , middleware.authentication  , middleware.authorisation, bookController.deleteBook)
 
-// router.get("/books/:bookId", authenticationMW.authorisation, bookController.getBook)
+router.post("/books/:bookId/review", reviewsController.reviews)
 
-// router.put("/books/:bookId", authenticationMW.authentication,authorisationMW, bookController.updateBook)
+router.put("/books/:bookId/review/:reviewId",reviewsController.updateReview)
+
+router.delete("/books/:bookId/review/:reviewId" , reviewsController.deleteReview)
+
+
 
 
 module.exports=router;
